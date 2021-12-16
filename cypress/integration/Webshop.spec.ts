3a442ff33489c3bb65f67920dbec2afe1890e9c0
Alexandrhall <alexander.hall@medieinstitutet.se>
2021-12-16 21:02:42 +0100

    ett till test

--- cypress/integration/Webshop.spec.ts
+++ cypress/integration/Webshop.spec.ts
@@ -10,4 +10,19 @@ describe("Webshop list test", () => {
 
         cy.get(".cartItem").should("have.length", 1);
     });
+    it("it should remove item in cart", () => {
+        cy.visit("http://localhost:1234");
+
+        cy.get("#continueButton").click();
+
+        cy.get(".buy-button:first").click();
+
+        cy.get("#prodCart").click();
+
+        cy.get(".cartItem").should("have.length", 1);
+
+        cy.get(".fa-trash-o").click();
+
+        cy.get(".cartItem").should("have.length", 0);
+    });
 });
